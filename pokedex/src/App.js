f1f7baab16960a46c6a3908592852745ea7bde7d
Vittorio Adesso <adessovittorio8@gmail.com>
2022-02-02 21:53:00 +0100

    Mejoras, funciones, ternarios

--- pokedex/src/App.js
+++ pokedex/src/App.js
@@ -8,12 +8,39 @@ function App() {
   // creo variables y les digo que serán un estado de acuerdo a lo ue traigan
   const [pokemonName, setPokemonName] = useState(""); 
 
-  // plantilla para llamar axios, estilo fecth 
+  //definir que información necesitamos del JSON y seterala en una variable
+  const [pokemonChosen, setPokemonChosen] = useState(false);
+
+  const [pokemon, setPokemon] = useState({
+
+    name: "",
+    number: "",
+    species: "",
+    image: "",
+    hp: "",
+    attack: "",
+    defense: "",
+    speed: "",
+    type: "",
+  });
+  
+  //plantilla, para que me muestre justo lo que yo le pido 
   const searchPokemon = () => {
     Axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
     .then(
-            (res) => {
-        console.log(res.data);
+      (res) => {
+        setPokemon({
+          name: pokemonName,
+          number: res.data.id,
+          species: res.data.species.name,
+          image: res.data.sprites.front_default,
+          hp: res.data.stats[0].base_stat,
+          attack: res.data.stats[1].base_stat,
+          defense: res.data.stats[2].base_stat,
+          speed: res.data.stats[5].base_stat,
+          type: res.data.types[0].type.name,
+        });
+        setPokemonChosen(true);
       }
     );
   };
@@ -23,13 +50,43 @@ function App() {
       <div className="TitleSection">
         <h1>Pokédex</h1>
         {/* configuro evento en el input */}
-        <input type="text" onChange={(event) => setPokemonName(event.target.value)} />
+        <input type="text" onChange={(event) => setPokemonName(event.target.value)   }
+        value={pokemonName.toLowerCase()}
+
+        />
 
         {/* doy funcionalidad al bontón */}
-        <button onClick={searchPokemon}>Search Pokémon</button>
+        <div>
+              {pokemonName && <button onClick={searchPokemon}>Search Pokémon</button>}
+            </div>
       </div>
+
+        <div className="DisplaySection">
+
+        {/* ternario en el return del componente */}
+        {!pokemonChosen ? (
+          <h1> Please choose a Pokémon </h1>
+        ) : (
+          <>
+            <h1>{pokemon.name}</h1>
+            <img src={pokemon.image} alt={pokemon.name} />
+            <h3>Number: #{pokemon.number}</h3>
+            <h3>Species: {pokemon.species}</h3>
+            <h3>Type: {pokemon.type}</h3>
+            <h4>Hp: {pokemon.hp}</h4>
+            <h4>Attack: {pokemon.attack}</h4>
+            <h4>Defense: {pokemon.defense}</h4>
+            <h4>Speed: {pokemon.speed}</h4>
+          </>
+        )}
+
+
+        </div>
+
+
+
     </div>
   );
-}
+}; 
 
 export default App;
