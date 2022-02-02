4b008a3da4352c096006bc00f3b29d28f659eeb2
Vittorio Adesso <adessovittorio8@gmail.com>
2022-02-02 21:37:46 +0100

    Axios, funcionalidad bontón, const, useState

--- pokedex/src/App.js
+++ pokedex/src/App.js
@@ -1,13 +1,32 @@
 import './App.css';
 import {useState} from "react";
+//una vez instalado npm install axios, lo importo 
+import Axios from "axios";
 
 function App() {
+
+  // creo variables y les digo que serán un estado de acuerdo a lo ue traigan
+  const [pokemonName, setPokemonName] = useState(""); 
+
+  // plantilla para llamar axios, estilo fecth 
+  const searchPokemon = () => {
+    Axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
+    .then(
+            (res) => {
+        console.log(res.data);
+      }
+    );
+  };
+
   return (
     <div className="App">
       <div className="TitleSection">
         <h1>Pokédex</h1>
-        <input type="text" />
-        <button>Search Pokémon</button>
+        {/* configuro evento en el input */}
+        <input type="text" onChange={(event) => setPokemonName(event.target.value)} />
+
+        {/* doy funcionalidad al bontón */}
+        <button onClick={searchPokemon}>Search Pokémon</button>
       </div>
     </div>
   );
